a5c90b00212776eec214e0845542cd7350e749a2
tmmons <t.m.mons.97@gmail.com>
2019-09-10 21:18:13 +0200

    Initial commit of the c# project

---
+++ plotter/plotter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace plotter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("This program will first make scatterplots of two chosen properties");
+
+            Console.ReadKey();
+        }
+    }
+}
