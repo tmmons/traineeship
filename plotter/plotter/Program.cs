c737bc419487d1110294d74247ec94147051c739
tmmons <t.m.mons.97@gmail.com>
2019-09-13 17:02:17 +0200

    Added Livecharts library for plotting, started work on 2 scatterplot methods

--- plotter/plotter/Program.cs
+++ plotter/plotter/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LiveCharts;
+using LiveCharts.Wpf;
 
 namespace plotter
 {
@@ -10,9 +12,44 @@ namespace plotter
     {
         static void Main(string[] args)
         {
-            Console.Write("This program will first make scatterplots of two chosen properties");
+            int nclusters = 10;
+            Console.WriteLine("Do you want to make a scatterplot, answer yes or no?");
+            string answer = Console.ReadLine();
+            if (answer == "yes" || answer == "Yes")
+            {
+                Console.WriteLine("This program will first make scatterplots of two chosen properties");
 
-            Console.ReadKey();
+                Console.WriteLine("Please enter the first properties to make a scatterplot of");
+                string property1 = Console.ReadLine();
+                string property2 = Console.ReadLine();
+
+                Console.WriteLine("Do you want to separate the data into clusters?");
+                answer = Console.ReadLine();
+                if (answer == "yes" || answer == "Yes")
+                {
+                    string SQL_query = "SELECT " + property1 + ", " + property2 + ", clusters FROM";
+                    double[,] values = new double[nclusters, 10000];
+
+                    for (int i = 0; i < data.Length(); i++)
+                    {
+
+                    }
+                }
+                else
+                {
+                    string SQL_query = "SELECT " + property1 + ", " + property2 + " FROM";
+
+                    values = new ChartValues<ObservablePoint>();
+
+                    for (int i = 0; i < data.Length(); i++)
+                    {
+                        values.Add(new ObservablePoint(data[i][0], data[i][1]));
+                    }
+                     public ChartValues<ObservablePoint> values { get; set; }
+                
+                }
+            }
+                   
         }
     }
 }
