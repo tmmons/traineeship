477907257950907184f7ecbe45c3d55e17ac037f
tmmons <t.m.mons.97@gmail.com>
2019-09-16 15:11:01 +0200

    Added correct SQL commands, still need working connection to SQL server

--- plotter/plotter/Program.cs
+++ plotter/plotter/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,8 @@ namespace plotter
         static void Main(string[] args)
         {
             int nclusters = 10;
+            string connectionstring = "";
+            string cnn = "";
             Console.WriteLine("Do you want to make a scatterplot, answer yes or no?");
             string answer = Console.ReadLine();
             if (answer == "yes" || answer == "Yes")
@@ -27,29 +31,58 @@ namespace plotter
                 answer = Console.ReadLine();
                 if (answer == "yes" || answer == "Yes")
                 {
-                    string SQL_query = "SELECT " + property1 + ", " + property2 + ", clusters WHERE IsNumeric("+property1+")=1 AND IsNumeric("+property2")=1 FROM exoplanets";
-                    double[,] values = new double[nclusters, 10000];
-
-                    for (int i = 0; i < data.Length(); i++)
+                    string SQL_query = "SELECT " + property1 + ", " + property2 + ", clusters WHERE IsNumeric(" + property1 + ")=1 AND IsNumeric(" + property2 + ")=1 FROM exoplanets";
+                    SqlCommand command = new SqlCommand(SQL_query, cnn);
+                    SqlDataReader reader;
+                    reader = command.ExecuteReader();
+                    List<double> data_ID = new List<double>();
+                    List<double> data_1 = new List<double>();
+                    List<double> data_2 = new List<double>();
+                    List<double> data_cluster = new List<double>();
+                    while (reader.Read())
                     {
+                        data_ID.Add(double(reader.GetValue(0)));
+                        data_1.Add(double(reader.GetValue(1)));
+                        data_2.Add(double(reader.GetValue(2)));
+                        data_cluster.Add(double(reader.GetValue(3)));
+                    }
+                    command.Dispose();
+                    cnn.Close();
 
+                    values = new ChartValues<ObservablePoint>();
+
+                    for (int i = 0; i < data_1.Length(); i++)
+                    {
+                        values.Add(new ObservablePoint(data_1[i], data_2[i]));
                     }
                 }
                 else
                 {
-                    string SQL_query = "SELECT " + property1 + ", " + property2 + WHERE IsNumeric("+property1+") = 1 AND IsNumeric("+property2")= 1 " FROM exoplanets";
+                    string SQL_query = "SELECT " + property1 + ", " + property2 + "WHERE IsNumeric(" + property1 + ") = 1 AND IsNumeric(" + property2 + ")= 1 FROM exoplanets";
+                    SqlCommand command = new SqlCommand(SQL_query, cnn);
+                    SqlDataReader reader;
+                    reader = command.ExecuteReader();
+                    List<double> data_ID = new List<double>();
+                    List<double> data_1 = new List<double>();
+                    List<double> data_2 = new List<double>();
+                    while (reader.Read())
+                    {
+                        data_ID.Add(double(reader.GetValue(0)));
+                        data_1.Add(double(reader.GetValue(1)));
+                        data_2.Add(double(reader.GetValue(2)));
+                    }
+                    command.Dispose();
+                    cnn.Close();
 
                     values = new ChartValues<ObservablePoint>();
 
-                    for (int i = 0; i < data.Length(); i++)
+                    for (int i = 0; i < data_1.Length(); i++)
                     {
-                        values.Add(new ObservablePoint(data[i][0], data[i][1]));
+                        values.Add(new ObservablePoint(data_1[i], data_2[i]));
                     }
-                     public ChartValues<ObservablePoint> values { get; set; }
-                
                 }
             }
-                   
         }
+                     
     }
 }
